cafdbc583d09cf442f4dd7d151ecc4f3500646fe
Ekaterina <1988snea@gmail.com>
2022-08-15 21:31:56 +0300

    Рефакторинг

--- src/react-APP/components/Footer/Footer.tsx
+++ src/react-APP/components/Footer/Footer.tsx
@@ -1,57 +1,53 @@
 
 
 import './footer.scss';
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { userUnset, userGetData } from '~s/actions/user';
-import { Link } from 'react-router-dom';
-import { routesMap } from '~r/routes';
-import { NavLink } from 'react-router-dom';
-import { Login } from '~c/Login/Login';
-import UserInfo from '~c/UserInfo/UserInfo';
-import { Form, Button, Modal } from 'react-bootstrap';
+import React from 'react';
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      <div className='footer__image'></div>
-      <div className='footer__info'>
-        <div className='footer__info-column'>
-          <header className='footer__info-column-header'>
-            ждем вас в наших ресторанах
-          </header>
-          <p className='footer__info-column-block'>
-            <span className='footer__info-column-row'>Уфа, центр</span>
-            <span className='footer__info-column-row'>Парковая, 1</span>
-            <span className='footer__info-column-row'>123-45-67</span>
-          </p>
-          <p className='footer__info-column-block'>
-            <span className='footer__info-column-row'>Уфа, Цветы Башкирии</span>
-            <span className='footer__info-column-row'>Цветочная, 1</span>
-            <span className='footer__info-column-row'>123-45-67</span>
-          </p>
-          <p className='footer__info-column-block'>
-            <span className='footer__info-column-row'>Уфа, р-н Молодежный</span>
-            <span className='footer__info-column-row'>Молодежная, 1</span>
-            <span className='footer__info-column-row'>123-45-67</span>
-          </p>
-        </div>
-        <div className='footer__info-column'>
-          <header className='footer__info-column-header'>
-            мы открыты для вас
-          </header>
-          <p className='footer__info-column-block'>
-            <span className='footer__info-column-row'>
-              понедельник - четверг
-            </span>
-            <span className='footer__info-column-row'>9:00 - 22:00</span>
-          </p>
-          <p className='footer__info-column-block'>
-            <span className='footer__info-column-row'>
-              пятница - воскресенье
-            </span>
-            <span className='footer__info-column-row'>11:00 - 24:00</span>
-          </p>
+      <div className='footer__wrapper'>
+        <div className='footer__image'></div>
+        <div className='footer__info'>
+          <div className='footer__info-column'>
+            <header className='footer__info-column-header'>
+              ждем вас в наших ресторанах
+            </header>
+            <p className='footer__info-column-block'>
+              <span className='footer__info-column-row'>Уфа, центр</span>
+              <span className='footer__info-column-row'>Парковая, 1</span>
+              <span className='footer__info-column-row'>123-45-67</span>
+            </p>
+            <p className='footer__info-column-block'>
+              <span className='footer__info-column-row'>Уфа, Цветы Башкирии
+              </span>
+              <span className='footer__info-column-row'>Цветочная, 1</span>
+              <span className='footer__info-column-row'>123-45-67</span>
+            </p>
+            <p className='footer__info-column-block'>
+              <span className='footer__info-column-row'>Уфа, р-н Молодежный
+              </span>
+              <span className='footer__info-column-row'>Молодежная, 1</span>
+              <span className='footer__info-column-row'>123-45-67</span>
+            </p>
+          </div>
+          <div className='footer__info-column'>
+            <header className='footer__info-column-header'>
+              мы открыты для вас
+            </header>
+            <p className='footer__info-column-block'>
+              <span className='footer__info-column-row'>
+                понедельник - четверг
+              </span>
+              <span className='footer__info-column-row'>9:00 - 22:00</span>
+            </p>
+            <p className='footer__info-column-block'>
+              <span className='footer__info-column-row'>
+                пятница - воскресенье
+              </span>
+              <span className='footer__info-column-row'>11:00 - 24:00</span>
+            </p>
+          </div>
         </div>
       </div>
     </footer>
--- src/react-APP/components/header/header.tsx
+++ src/react-APP/components/header/header.tsx
@@ -2,14 +2,11 @@
 
 import './header.scss';
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { userUnset, userGetData } from '~s/actions/user';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { routesMap } from '~r/routes';
 import { NavLink } from 'react-router-dom';
-import { Login } from '~c/Login/Login';
 import UserInfo from '~c/UserInfo/UserInfo';
-import { Form, Button, Modal } from 'react-bootstrap';
 
 interface IRoutesMap {
   [index: string]: { name: string; img?: any };
@@ -19,11 +16,10 @@ const Header = () => {
   const props: any = useSelector(state => {
     return state;
   });
-  const dispatch = useDispatch();
 
-  const [isInfoShown, changeInfoShown] = useState(false);
-
-  const email = props.user.user.email;
+  const [details, setDetails] = useState(false);
+  const burgerClassName = details ? 'header__burger-button_active' : '';
+  const burgerClasses = ['header__burger-button', burgerClassName];
   const name = props.user.user.name;
   const surname = props.user.user.surname;
   const isAuthed = !!props.user.user.token;
@@ -69,15 +65,13 @@ const Header = () => {
   const pizzaItemsIMG = require('./img/logo.png').default;
   const list: IRoutesMap =
   {
-    pizzaItems: { name: 'Главная', img: pizzaItemsIMG },
     about: { name: 'О нас' },
     deals: { name: 'Акции' },
     blog: { name: 'Блог' },
   };
   for (let key in routesMap) {
     if (key in list) {
-      const hasImg = !!list[key].img;
-      const name = hasImg ? '' : list[key].name;
+      const name = list[key].name;
       const menuItem = (
         <li
           className='header__nav-item'
@@ -96,24 +90,39 @@ const Header = () => {
     }
   }
 
-
-
   return (
     <header className='header'>
-      <nav className='header__nav'>
-        <ul className='header__nav-list'>
-          {menuItems}
-        </ul>
-      </nav>
-      <div className='header__user'>
-        {loginForm}
-        {cart}
+      <button
+        aria-label="навигация"
+        className={burgerClasses.join(' ')}
+        onClick={() => setDetails(prev => !prev)}
+      >
+        <div
+          className='header__burger'>
+        </div>
+      </button>
+      <div className='header__wrapper'>
+        <div className='header__logo'>
+          <NavLink
+            className='header__nav-link'
+            to={routesMap.pizzaItems}>
+            <img
+              className='header__nav-img'
+              src={pizzaItemsIMG} alt="" />
+          </NavLink>
+        </div>
+        <nav className='header__nav'>
+          <ul className='header__nav-list'>
+            {menuItems}
+          </ul>
+        </nav>
+        <div className='header__user'>
+          {loginForm}
+          {cart}
+        </div>
       </div>
     </header>
   );
-
-
-
 };
 
 export { Header };
